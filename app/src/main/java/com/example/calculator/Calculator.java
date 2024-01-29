81be623968e8f01accd66b94e7d6424694dbd577
Beeranna Dude <101712802+beeru405@users.noreply.github.com>
2024-01-29 20:05:22 +0530

    Create Calculator.java

---
+++ app/src/main/java/com/example/calculator/Calculator.java
@@ -0,0 +1,23 @@
+package com.example.calculator;
+
+public class Calculator {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static double divide(double a, double b) {
+        if (b != 0) {
+            return a / b;
+        } else {
+            throw new ArithmeticException("Cannot divide by zero");
+        }
+    }
+}
